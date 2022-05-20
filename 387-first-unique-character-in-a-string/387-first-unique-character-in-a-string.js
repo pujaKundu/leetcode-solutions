346135f223b3ede23567e953e11305f850515578
Puja Kundu <puja.kundu.cs@gmail.com>
2022-05-20 13:57:04 +0600

    Time: 131 ms (58.22%), Space: 45 MB (82.54%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    if(s.length ===0)
+        return -1;
+    for(let i=0;i<s.length;i++){
+        const letter = s[i];
+        if(s.indexOf(letter) === s.lastIndexOf(letter))
+            return i;
+    }
+    return -1;
+};
